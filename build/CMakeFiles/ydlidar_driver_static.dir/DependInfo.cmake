30b0d4417b7cb1f63f46da676e7e9b805f1741a4
Kevin Joaquim <kevin.marques.joaquim@gmail.com>
2019-04-28 18:04:27 +0200

    first commit

---
+++ build/CMakeFiles/ydlidar_driver_static.dir/DependInfo.cmake
@@ -0,0 +1,52 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/kevin/Bureau/m2/robot/SDK g4/SDK/ydlidar_sdk/Serial/lock.c" "/home/kevin/Bureau/m2/robot/SDK g4/SDK/build/CMakeFiles/ydlidar_driver_static.dir/Serial/lock.c.o"
+  "/home/kevin/Bureau/m2/robot/SDK g4/SDK/ydlidar_sdk/simpleini/ConvertUTF.c" "/home/kevin/Bureau/m2/robot/SDK g4/SDK/build/CMakeFiles/ydlidar_driver_static.dir/simpleini/ConvertUTF.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "YDLIDAR_API_EXPORTS"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "/home/kevin/Bureau/m2/robot/SDK g4/SDK/ydlidar_sdk"
+  "."
+  "/home/kevin/Bureau/m2/robot/SDK g4/SDK/ydlidar_sdk/Lidar"
+  "/home/kevin/Bureau/m2/robot/SDK g4/SDK/ydlidar_sdk/Serial"
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/kevin/Bureau/m2/robot/SDK g4/SDK/ydlidar_sdk/Lidar/Drivers/YDLidar/unix_timer.cpp" "/home/kevin/Bureau/m2/robot/SDK g4/SDK/build/CMakeFiles/ydlidar_driver_static.dir/Lidar/Drivers/YDLidar/unix_timer.cpp.o"
+  "/home/kevin/Bureau/m2/robot/SDK g4/SDK/ydlidar_sdk/Lidar/Drivers/YDLidar/ydlidar_driver.cpp" "/home/kevin/Bureau/m2/robot/SDK g4/SDK/build/CMakeFiles/ydlidar_driver_static.dir/Lidar/Drivers/YDLidar/ydlidar_driver.cpp.o"
+  "/home/kevin/Bureau/m2/robot/SDK g4/SDK/ydlidar_sdk/Serial/impl/unix/list_ports_linux.cpp" "/home/kevin/Bureau/m2/robot/SDK g4/SDK/build/CMakeFiles/ydlidar_driver_static.dir/Serial/impl/unix/list_ports_linux.cpp.o"
+  "/home/kevin/Bureau/m2/robot/SDK g4/SDK/ydlidar_sdk/Serial/impl/unix/unix_serial.cpp" "/home/kevin/Bureau/m2/robot/SDK g4/SDK/build/CMakeFiles/ydlidar_driver_static.dir/Serial/impl/unix/unix_serial.cpp.o"
+  "/home/kevin/Bureau/m2/robot/SDK g4/SDK/ydlidar_sdk/Serial/serial.cpp" "/home/kevin/Bureau/m2/robot/SDK g4/SDK/build/CMakeFiles/ydlidar_driver_static.dir/Serial/serial.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "YDLIDAR_API_EXPORTS"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/kevin/Bureau/m2/robot/SDK g4/SDK/ydlidar_sdk"
+  "."
+  "/home/kevin/Bureau/m2/robot/SDK g4/SDK/ydlidar_sdk/Lidar"
+  "/home/kevin/Bureau/m2/robot/SDK g4/SDK/ydlidar_sdk/Serial"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
